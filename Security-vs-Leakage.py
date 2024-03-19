7c281f55141fdb0ad1aae4d4e52864958c1146f2
mderee <markderee@gmail.com>
2024-03-19 18:55:15 +0100

    Update
    The script has been updated to represent a simple function that estimates the bit security that can be achieved by an established bit sequence of particular length and for a particular adversarial bit inference rate.

--- Security-vs-Leakage.py
+++ Security-vs-Leakage.py
@@ -1,39 +1,47 @@
-import math
+"""
+The function Bit_Security_Estimation estimates how much bit security a bit sequence (e.g., cryptographic key) achieves
+under the assumption that an adversary can determine the value of bits within the bit sequence with a probability
+strictly greater than 50% and strictly lower than 100%. The adversary may enjoy such an advantage when the bit sequence
+establishment (e.g., key establishment) process leaks information against a particular side-channel attack.
 
-# How many guesses does an adversary, on average, have to try to find the correct key (probability 2^-n)?
-# Input parameters (adversarial bit estimation accuracy, range of key lengths).
-est_Accuracy = 0.6
-min_keySize = 1
-max_keySize = 514
+The function takes as inputs the bit sequence length (seqLength) and the adversarial bit inference rate (inferenceRate).
+The function determines the computational effort required by an adversary, launching a key search attack by trialing
+keys (i.e., bit sequences) in order of decreasing probability. The adversary goes through an iterative process where
+it first trials the most likely key (i.e., the bit sequence where each bit is the adversary's inferred bit) and
+subsequent iterations trials the set of keys where an additional bit is assumed incorrectly inferred.
 
-# Computes the Bit Security level for all key lengths in the pre-selected range.
-for keySize in range(min_keySize, max_keySize + 1):
+This algorithm is covered and featured in the published research paper below:
+M. de Ree et al., "Bit Security Estimation for Leakage-Prone Key Establishment Schemes," IEEE Communications Letters,
+vol. 27, no. 7, 2023. [DOI: 10.1109/LCOMM.2023.3275647]
+"""
 
-    # Resets the index of the guessed keys.
+def Bit_Security_Estimation(seq_length, inference_rate):
+    import math
+
+    guesses = 0
     sum_upperBound = 0
 
-    # Resets the average number of guesses that an adversary needs to try to find the correct key.
-    guesses = 0
-
-    for bits_false in range(0, keySize + 1):
-        #Prepares the starting index for the guessed keys.
+    for bits_false in range(seq_length):
+        # Prepares the starting index for the guessed keys.
         sum_lowerBound = sum_upperBound + 1
     
         # Estimates the number of keys of length [n] in which [0 to n] bits were guessed (in)correctly
         # (i.e., binomial coefficient).
-        trialed_keys_in_iteration = math.comb(keySize, bits_false)
+        trialed_keys_in_iteration = math.comb(seq_length, bits_false)
 
         # Sets and sums the indexes of the guessed keys within the current set of most likely keys.
         sum_upperBound = sum_upperBound + trialed_keys_in_iteration
         ordinality_summation = (sum_lowerBound + sum_upperBound) * (sum_upperBound - sum_lowerBound + 1) / 2
 
         # Increases the number of guesses required by the adversary from trying the set of most likely, remaining, keys.
-        guesses = guesses + ordinality_summation * est_Accuracy**(keySize-bits_false) * (1-est_Accuracy)**bits_false
+        guesses = guesses + ordinality_summation * inference_rate ** (seq_length - bits_false) * (1 - inference_rate) ** bits_false
 
         # Prepares the indexes for the next round of adversarial guesses.
         sum_lowerBound = sum_lowerBound + trialed_keys_in_iteration
 
-    # Computes and prints the bit security based on the average number of guesses that an adversary has to try to
-    # find the correct key.
-    bitSecurity = math.log2(2*guesses-1)
-    print("Key size " + str(keySize) + " with bit-sec: " + str(bitSecurity))
+    # Determine the bit security from the average number of guesses required by an adversary to find the correct key.
+    return math.log2(2 * (guesses - 1))
+
+
+# Example
+print(Bit_Security_Estimation(seq_length = 128, inference_rate = 0.6))
